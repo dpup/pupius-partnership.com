c5fc1debcdfc1ede2580d1d4108e850b7e1e0bb0
Dan Pupius <dan@medium.com>
2014-08-31 19:22:43 -0700

    Image sizing

--- www/index.php
+++ www/index.php
@@ -11,8 +11,24 @@
     padding-bottom: 50px;
   }
 
-  img {
+  .image {
+    position: relative;
     width: 100%;
+    height: 0;
+    padding: 40% 0 0 0;
+    overflow: hidden;
+  }
+
+  img {
+    position: absolute;
+    display: block;
+    max-width: 100%;
+    max-height: 800px;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
   }
 
   h1, h2, p {
@@ -57,7 +73,7 @@
 </style>
 </head>
 <body>
-  <img src="peaks.jpg">
+  <div class="image"><img src="peaks.jpg"></div>
 
   <h1>The Pupius Partnership</h1>
   <h2>
